dc9b2d707b11dcbd65b7dde507a173fb8bcef4e8
chbbs231 <chbbs231@yahoo.com>
2020-02-18 12:46:00 -0800

    first page working

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
-
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+//Import for pages
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Portfolio from './pages/Portfolio'
+//Import for components
+import Nav from './components/Nav'
 
 function App() {
   return (
-    <div></div>
+    <Router>
+    <div>
+      <Switch>
+        <Route exact path ="/">
+          <About/>
+        </Route>
+        <Route path= "/portfolio">
+          <Portfolio/>
+        </Route>
+        <Route path= "/contact">
+          <Contact/>
+        </Route>
+      </Switch>
+    </div>
+    </Router>
   )
 }
 
